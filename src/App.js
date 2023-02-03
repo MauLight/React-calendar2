59b9ed2b60b531b5fede14ab8e0a8b858d2ad6f7
MauLight <maulisseluz@gmail.com>
2023-02-03 06:27:11 -0300

    calendar not working, fetch api broken

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { gapi } from "gapi-script";
+import Event from "./components/Event";
+
 
 function App() {
+
+  const [events, setEvents] = useState([]);
+
+  const calendarID = "wakeup.mau@gmail.com";
+  const apiKey = "AIzaSyAWOSQ00eTh9fplH6yhsXDtd7vzDtOHv4A";
+  const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN;
+
+  const getEvents = (calendarID, apiKey) => {
+    function initiate() {
+      gapi.client.init({
+        apiKey: apiKey
+      })
+
+        .then(function () {
+          return gapi.client.request({
+            path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
+          })
+        })
+
+        .then((response) => {
+          let events = response.results.items;
+          return events;
+        },
+          function (err) {
+            return [false, err];
+          }
+        );
+    }
+
+    gapi.load("client", initiate);
+
+  };
+
+  useEffect(() => {
+    const events = getEvents(calendarID, apiKey);
+    setEvents(events);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App py-8 flex flex-col justify-center">
+      <h1 className="text-2xl font-bold mb-4">Calendar</h1>
+      <ul>
+        {
+          events?.map((event) => (
+            <li key={event.id} className="flex justify-center">
+              <Event description={event.summary} />
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
